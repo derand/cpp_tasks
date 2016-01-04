35e19865a8352d14d3f862577d73407294ebbc34
Andrey Derevyagin <2derand@gmail.com>
2016-01-04 22:25:11 +0200

    Loops and conditions (end)

---
+++ task0208.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    float a, b, c, x_start, x_end, x_d;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    cout << "Enter c:";
+    cin >> c;
+    cout << "Enter x_start:";
+    cin >> x_start;
+    cout << "Enter x_end:";
+    cin >> x_end;
+    cout << "Enter d_x:";
+    cin >> x_d;
+    x_d = fabs(x_d);
+
+    float f, x;
+    if (x_start > x_end) {
+        float tmp = x_start;
+        x_start = x_end;
+        x_end = tmp;
+    }
+    cout << "X\tF\n";
+    x = x_start;
+    while (x < x_end) {
+        if (c < 0 && fabs(a) > 0.00001) {
+            f = -a*x*x; 
+        } else if (c > 0 && fabs(a) < 0.00001) {
+            f = (x - a) / (x * c);
+        } else {
+            f = x / c;
+        }
+        cout << x << '\t' << f << '\n';
+        x += x_d;
+    }
+
+    return 0;
+}
---
+++ task0212.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    float a, b, c, x_start, x_end, x_d;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    cout << "Enter c:";
+    cin >> c;
+    cout << "Enter x_start:";
+    cin >> x_start;
+    cout << "Enter x_end:";
+    cin >> x_end;
+    cout << "Enter d_x:";
+    cin >> x_d;
+    x_d = fabs(x_d);
+
+    float f, x;
+    if (x_start > x_end) {
+        float tmp = x_start;
+        x_start = x_end;
+        x_end = tmp;
+    }
+    cout << "X\tF\n";
+    x = x_start;
+    while (x < x_end) {
+        if (x < 0.6 && fabs(b+c) > 0.00001) {
+            f = a*pow(x, 3.0) + b*b + c; 
+        } else if (x > 0 && fabs(b+c) < 0.00001) {
+            f = (x - a) / (x - c);
+        } else {
+            f = x / c + x / a;
+        }
+        cout << x << '\t' << f << '\n';
+        x += x_d;
+    }
+
+    return 0;
+}
