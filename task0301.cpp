409abb6799744cec5c8f05eae2bbd82fdf243a74
Andrey Derevyagin <2derand@gmail.com>
2016-01-06 00:31:27 +0200

    Function calculation

---
+++ task0301.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    const int iter_max = 500;
+    const double eps = 0.00000001;
+    int i = 0;
+    double x;
+
+    cout << "Enter X: ";
+    cin >> x;
+
+    double val = 2 / x;
+    double y = val; 
+    while (i < iter_max && fabs(val) >= eps) {
+        val *= (2*i + 1) / ((2*i + 3) * x * x);
+        y += val;
+        i++;
+    }
+
+    if (fabs(val) < eps) {
+        cout << "y=" << y << "\nIterations:" << i << '\n';
+    } else {
+        cout << "Reach maximum iteration count.\n";
+    }
+
+    return 0;
+}
